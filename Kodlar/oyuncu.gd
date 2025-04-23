be46584c355176816466c38f24bd16d2d4eb7e82
Abdulkadir Yeşilkaya <abdulkadir7725@gmail.com>
2025-04-23 21:28:21 +0300

    iki kişilik oynanılacak hale geitirildi

--- Kodlar/oyuncu.gd
+++ Kodlar/oyuncu.gd
@@ -5,10 +5,10 @@ extends CharacterBody2D
 #Sahne veya oyun devam ettiği sürece tekrarla
 func _physics_process(delta: float) -> void:
 	velocity = Vector2.ZERO
-	if Input.is_action_pressed("yukarı"):
+	if Input.is_action_pressed("w"):
 		#print("Yukarı Gitti")
 		velocity.y = velocity.y - 1
-	if Input.is_action_pressed("aşağı"):
+	if Input.is_action_pressed("s"):
 		#print("Aşağı Gitti")
 		velocity.y = velocity.y + 1
 	move_and_collide(velocity * karakterHiz)
