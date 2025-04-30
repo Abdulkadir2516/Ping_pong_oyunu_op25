422f985a9ea43db2648cbc79b10fce43bcc85cbd
Abdulkadir Yeşilkaya <abdulkadir7725@gmail.com>
2025-04-30 20:37:15 +0300

    oyuncu 2 sahnesi oluşturuldu

--- Kodlar/oyuncu2.gd
+++ Kodlar/oyuncu2.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
-#var top
+var top
 
-#func _ready() -> void:
+func _ready() -> void:
 	
-	#top = get_parent().get_node("Top")
+	top = get_parent().get_node("Top")
 	
 #Sahne veya oyun devam ettiği sürece tekrarla
 
@@ -13,21 +13,21 @@ extends CharacterBody2D
 #Sahne veya oyun devam ettiği sürece tekrarla
 func _physics_process(delta: float) -> void:
 	velocity = Vector2.ZERO
-	if Input.is_action_pressed("yukarı"):
-		#print("Yukarı Gitti")
-		velocity.y = velocity.y - 1
-	if Input.is_action_pressed("aşağı"):
-		#print("Aşağı Gitti")
-		velocity.y = velocity.y + 1
-	move_and_collide(velocity * karakterHiz)	
+	#if Input.is_action_pressed("yukarı"):
+		##print("Yukarı Gitti")
+		#velocity.y = velocity.y - 1
+	#if Input.is_action_pressed("aşağı"):wsw
+		##print("Aşağı Gitti")
+		#velocity.y = velocity.y + 1
+	#move_and_collide(velocity * karakterHiz)	
 	
-	#move_and_collide(Vector2(0,yon_belirle()) * karakterHiz)
+	move_and_collide(Vector2(0,yon_belirle()) * karakterHiz)
 
-#func yon_belirle():	
-	#if abs(top.position.y - position.y) > 35:
-		#if top.position.y > position.y:
-			#return 1
-		#else:
-			#return -1
-	#else:
-		#return 0
+func yon_belirle():	
+	if abs(top.position.y - position.y) > 35:
+		if top.position.y > position.y:
+			return 1
+		else:
+			return -1
+	else:
+		return 0
